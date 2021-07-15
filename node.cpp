72bbfa9ab9de2dd7cd5d1bee9e8709cbf4cefcc7
MostafaAbdelfadil <mostafaabdelfadil81@gmail.com>
2021-07-15 06:11:16 +0200

    Node Structure

--- node.cpp
+++ node.cpp
@@ -1,1 +1,23 @@
 #include "node.h"
+
+node::node(string t)
+{
+	name = t;
+	JsonName = '"' + name.substr(1, name.length() - 2) + '"';
+	JsonMark = false;
+	level = 0;
+	parent = NULL;
+}
+
+void node::set_attributes(vector<string> toks)
+{
+	for (int i = 1; i < toks.size(); i++)
+	{
+		attribute tr;
+		int equal = toks[i].find('=');
+		tr.name = '"' + toks[i].substr(0, equal) + '"';
+		tr.value = toks[i].substr(equal + 1);
+		attrs.push_back(tr);
+	}
+}
+
--- node.h
+++ node.h
@@ -1,5 +1,44 @@
 #pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+//attribute
+struct attribute
+{
+	string name;
+	string value;
+	int level;
+};
+
 class node
 {
+	string tag;   //tag with attributes
+	string name;  //tag without attributes
+	string JsonName;
+	bool JsonMark;
+	vector<attribute> attrs;
+	int level;
+	node* parent;
+	vector<node*> childs;
+public:
+	node() { level = 0; JsonMark = false; parent = NULL; }
+	node(string);
+	void set_tag(string g) { tag = g; }
+	string get_tag() { return tag; }
+	void set_name(string n) { name = n; JsonName = '"' + name.substr(1, name.length() - 2) + '"'; }
+	string get_name() { return name; }
+	void set_JsonName(string j) { JsonName = '"' + j + '"'; }
+	string get_JsonName() { return JsonName; }
+	void set_Mark(bool m) { JsonMark = m; }
+	bool get_Mark() { return JsonMark; }
+	void set_attributes(vector<string>);
+	vector<attribute> get_attributes() { return attrs; }
+	void set_lvl(int l) { level = l; if (attrs.size() > 0) { for (int i = 0; i < attrs.size(); i++) { attrs[i].level = l + 1; } } }
+	int get_lvl() { return level; }
+	void set_parent(node* p) { parent = p; }
+	node* get_parent() { return parent; }
+	void set_child(node* ch) { childs.push_back(ch); childs.back()->set_parent(this); }
+	vector<node*> get_childs() { return childs; }
 };
 
