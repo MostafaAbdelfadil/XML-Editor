af885c644995d385ab354fbe593df725df60e384
Abdelrahman710 <1700759@eng.asu.edu.eg>
2021-07-15 05:20:30 +0200

    function to Collect similar nodes in the same level

--- Tree.cpp
+++ Tree.cpp
@@ -1,1 +1,27 @@
 #include "Tree.h"
+vector<vector<node*>> Tree::similar(vector<node*> same)
+{
+	vector<vector<node*>> a;
+	for (int i = 0; i < same.size(); i++)
+	{
+		vector<node*> temp;
+		if (!same[i]->get_Mark())
+		{
+			temp.push_back(same[i]);
+			same[i]->set_Mark(true);
+		}
+		for (int j = i + 1; j < same.size(); j++)
+		{
+			if ((same[i]->get_name() == same[j]->get_name()) && (!same[j]->get_Mark()))
+			{
+				temp.push_back(same[j]);
+				same[j]->set_Mark(true);
+			}
+		}
+		if (temp.size() > 0)
+			a.push_back(temp);
+	}
+	for (int i = 0; i < same.size(); i++)
+		same[i]->set_Mark(false);
+	return a;
+}
